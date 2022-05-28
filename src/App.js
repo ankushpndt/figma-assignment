63fd41389455c5ec000212efa7a32c374c9add57
Ankush Sharma <ankushpndt@gmail.com>
2022-05-29 03:14:15 +0530

    fix:resolved a bug

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React from "react";
 import "./App.css";
 import InputList from "./components/Users/InputList";
 import NewMessageOverview from "./components/Users/NewMessageOverview";
-import { Routes, Route } from "react-router-dom";
-
+import { Routes, Route, Link } from "react-router-dom";
+import MessagesOverview from "./components/Users/MessagesOverview";
 const App = () => {
 	return (
 		<div className="App">
-			<nav className="navbar">Smarter.Codes</nav>
+			<nav className="navbar">
+				<Link to="/" style={{ color: "white", textDecoration: "none" }}>
+					Smarter.Codes
+				</Link>
+			</nav>
 			<div style={{ display: "flex" }}>
 				<div className="components">
 					<InputList />
 				</div>
 
 				<Routes>
+					<Route path="/" element={<MessagesOverview />} />
 					<Route path="/inputlist" element={<InputList />} />
 					<Route
 						path="/newmessageoverview/:userId"
